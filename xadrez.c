1d28c8d021d517d478d810a05368736311434025
marco9813 <106922930+marco9813@users.noreply.github.com>
2025-05-08 18:32:47 +0000

    xadrez com recursiva e loops aninhados

--- xadrez.c
+++ xadrez.c
@@ -1,35 +1,48 @@
 #include <stdio.h>
 
+void moverTorre(int casas){
+    if (casas > 0){ // aqui é a quantidade de vezes que o código vai rodar
+        printf("Direita \n");
+        moverTorre(casas - 1); // aqui vai decrementar o valor de casas em 1
+    }
+}
 
+void moverBispo(int casas){
+    if (casas > 0){ // aqui vai ser a quantidade de vezes que o código vai rodar
+        for(int i = 1; i <= 1 ; i++){ // aqui o i é 1 e vai rodar 1 vez
+            for(int j = 0; j < i; j++){ // aqui o j é 0 e também vai rodar 1 vez
+                printf("Direita \n");
+            }
+            printf("Cima \n");
+        }
+        moverBispo(casas - 1); // quando a condição do for externo for falsa executa a recursiva
+    }
+}
+
+void moverRainha(int casas){
+    if(casas > 0){
+        printf("Esquerda \n");
+        moverRainha(casas - 1);
+    }
+}
 
 int main() {
-    // Movendo a torre 5 casas para direita
-    for(int i = 1; i <= 5; i++){// o programa vai executar o código 1 até 5 até a condição ser falsa e parar o loop
-        printf("Direita\n");
-    }
+    // movimento da torre
+    moverTorre(5);
     // Movendo o bispo 5 casas para cima, direita
-    int b = 1;
-    while (b <= 5){// o loop continua até que a condição seja falsa e o loop termina
-        printf("Cima, Direita\n");// vai imprimir 5 vezes
-        b++;
-    }
+    moverBispo(5);
     // movendo a rainha 8 casas a esquerda
-    int r = 1;
-    do{
-        printf("Esquerda\n");// imprime pela 1 vez
-        r++;// imcrementa
-
-    }while ( r <= 8);// verifica a condição se for verdadeira continua o loop, se for falsa o loop termina
+    moverRainha(8);
 
     // Movimento do Cavalo
     int movimento = 1;
     
     while (movimento--){ // aqui vai executar 1 vez, depois faz o decremento aonde o movimento se torna 0 ai a condição é falsa e termina a execução
 
-        for (int i = 1; i <= 2; i++){
-            printf("Baixo\n"); // executa o printf duas vezes
+        for (int i = 1, j = 2; i <= j; i++){ // aqui temos multiplas variaveis aonde a condição va ser verdadeira duas vezes
+            printf("Cima \n"); // executa o printf duas vezes
         }
-        printf("Esquerda\n");// executa 1 vez
+        printf("Direita \n");// executa 1 vez
     }
 
     return 0;
